4cf4a330e9e76503fd20624e38f006cc24f7955c
Sameer Dhanda <78817767+sd12354@users.noreply.github.com>
2026-09-01 13:56:07 -0400

    Add initial developer profile program

---
+++ app.py
@@ -0,0 +1,10 @@
+def main():
+    print("Senior Project Developer Profile")
+    print()
+    print("Name: Sameer Dhanda")
+    print("Major: Computer Science")
+    print("Technology Interest: Artificial Intelligence")
+    print("Skill Goal: Full-Stack Web Development")
+
+if __name__ == "__main__":
+    main()
